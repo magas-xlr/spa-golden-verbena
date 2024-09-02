83bed27126b22bc3c02ba35d95563c315d7faa96
mathbrss <matheusbrs2020@gmail.com>
2024-09-02 20:28:30 -0300

    add error message

--- script.js
+++ script.js
@@ -3,12 +3,15 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const btnEnviar = document.querySelector('button');
     const radios = document.querySelectorAll('input[name="voto"]');
+    const mensagemErro = document.getElementById('mensagemErro');
     function verificarSelecao() {
         const Selecionada = Array.from(radios).some(radio => radio.checked);
         if (Selecionada) {
             btnEnviar.disabled = false;
+            mensagemErro.style.display = 'none'; // Esconde a mensagem de erro quando uma opção é selecionada
         } else {
             btnEnviar.disabled = true;
+            mensagemErro.style.display = 'block'; // Mostra a mensagem de erro quando nenhuma opção é selecionada
         }
     }
     radios.forEach(radio => {
