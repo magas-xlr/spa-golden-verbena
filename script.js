5e68a70a6d9fcab24b2fde86dcb13fb60604ce43
mathbrss <matheusbrs2020@gmail.com>
2024-09-22 11:21:27 -0300

    fix(SGV06): convert options to json data

--- script.js
+++ script.js
@@ -1,14 +1,54 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const options = [
+        {
+            description: 'Ótimo',
+            id: 'otimo',
+        },
+        {
+            description: 'Bom',
+            id: 'bom',
+        },
+        {
+            description: 'Não gostei',
+            id: 'nao-gostei',
+        },
+    ];
+
+    const container = document.getElementById('enquete_options');
+
+    options.forEach(option => {
+        // Criando o input (radio button)
+        const input = document.createElement('input');
+        input.type = 'radio';
+        input.name = 'voto';
+        input.id = options.id;
+        input.value = options.id;
+        input.required;
+
+        // Criando o label
+        const label = document.createElement('label');
+        label.htmlFor = option.id;
+        label.textContent = option.description;
+
+        // Adicionando os elementos ao container
+        container.appendChild(input);
+        container.appendChild(label);
+        container.appendChild(document.createElement('br'));
+    });
+
+
+
 function erroMessage() {
     const radioSelected = document.querySelectorAll('input[name="voto"]');
-    const option = false;
+    const options = false;
 
     radioSelected.forEach((radio) => {
         if (radio.checked) {
-            option = true;
+            options = true;
         }
     });
 
-    if (!option) {
+    if (!options) {
         const strong = document.createElement('strong');
         const text = document.createTextNode('Selecione uma opção antes de enviar.');
         strong.appendChild(text);
@@ -52,4 +92,6 @@ document.querySelectorAll('input[name="voto"]').forEach(radio => {
     radio.addEventListener('change', () => {
         checkSelection(radio);
     });
+});
+
 });
