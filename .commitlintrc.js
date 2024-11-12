ee976cb6842a6cc49399f1b7bacea62fbe9489fc
mathbrss <matheusbrs2020@gmail.com>
2024-11-12 09:46:24 -0300

    chore(sgv08): setup of Husky, Commitizen, and Commitlint for standardized commit control
    Setting up Husky, Commitizen, and Commitlint to enforce standardized commit messages across the
    project. This configuration integrates Husky hooks to automate the commit message validation
    process, Commitizen to provide an interactive prompt for creating conventional commits, and
    Commitlint to check that all commit messages follow a specific format. Together, these tools ensure
    consistent and readable commit history, improving collaboration and simplifying project maintenance.

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,8 +1,7 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
-      'scope-enum': [2, 'always', [/^SGV-\d+$/]],
-      'scope-empty': [2, 'never'], 
+      'scope-enum': [2, 'always', [/^SGV-\d+$/]], // Valida o padrão SGV-NÚMERO com uma expressão regular, sendo "d" dígitos decimais
+      'scope-empty': [2, 'never'], // Exige que o scope não esteja vazio
     },
-  };
-  
+};
