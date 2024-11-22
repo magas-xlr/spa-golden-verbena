96cf42d4b333fe7a5e7217fbf59b525cd7f96072
mathbrss <matheusbrs2020@gmail.com>
2024-11-22 18:06:12 -0300

    chore: add eslint config

--- .commitlintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-      'scope-enum': [2, 'always', [/^SGV-\d+$/]], // Valida o padrão SGV-NÚMERO com uma expressão regular, sendo "d" dígitos decimais
-      'scope-empty': [2, 'never'], // Exige que o scope não esteja vazio
-    },
-};
